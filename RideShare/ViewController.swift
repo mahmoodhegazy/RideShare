ed21e3039403be6284288c2988f1daeda5d9a123
Guzlan <amro.guzlan@gmail.com>
2016-06-16 23:13:50 -0400

    Initial Commit

---
+++ RideShare/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RideShare
+//
+//  Created by Amr Guzlan on 2016-06-16.
+//  Copyright © 2016 Amro Gazlan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
